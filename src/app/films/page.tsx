24c02f7498a6b131e180f4c8a2551df81f7d8e89
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-10-10 15:11:39 +0530

    films section done

---
+++ src/app/films/page.tsx
@@ -0,0 +1,12 @@
+import OurFilms from "@/components/HeroSection/OurFIlms";
+import React from "react";
+
+export default function Films() {
+  return (
+    <div className="min-h-screen justify-center items-center flex flex-col w-full py-12 mt-12">
+      <div className="container">
+        <OurFilms />
+      </div>
+    </div>
+  );
+}
